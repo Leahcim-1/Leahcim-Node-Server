c6ad5f839c166ee22b51bd20ca0aef045e21c443
leahcim-1 <xinhaosu@126.com>
2020-03-13 20:28:48 +0800

    Testing for deploying in google server

--- server.js
+++ server.js
@@ -2,15 +2,8 @@ var express = require('express');
 var routers = require('./routers/routers')
 var app = express();
 
-
-
  app.use('/static', express.static('static'));
  app.use(routers)
- 
- app.use((req, res, next) => {
-        console.log('token sign')
-
-    })
  
 
 var server = app.listen(20000, function ( ) {
